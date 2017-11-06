a5fce63db39632e7add5cdad5314fe0f44bd5490
Muhammad Saiful Islam <muhammad@saiful.web.id>
2017-11-06 15:29:13 +0700

    postman: Add more data to export
    At this level, our exported JSON is compatible with Postman.

--- postman/schema.go
+++ postman/schema.go
@@ -3,11 +3,34 @@ package postman
 // https://schema.getpostman.com/json/collection/v2.1.0/docs/index.html
 
 type Collection struct {
-	Info Information `json:"info"`
+	Info  Information `json:"info"`
+	Items []Folder    `json:"item,omitempty"`
 }
 
 type Information struct {
 	Name      string `json:"name"`
-	PostmanID string `json:"_postman_id",omitempty`
+	PostmanID string `json:"_postman_id,omitempty"`
 	Schema    string `json:"schema"`
+}
+
+type Folder struct {
+	Name  string      `json:"name,omitempty"`
+	Items interface{} `json:"item"`
+}
+
+type Item struct {
+	Name    string  `json:"name,omitempty"`
+	Request Request `json:"request"`
+}
+
+type Request struct {
+	Url    string   `json:"url,omitempty"`
+	Method string   `json:"method,omitempty"`
+	Header []Header `json:"header,omitempty"`
+}
+
+type Header struct {
+	Key         string `json:"key"`
+	Value       string `json:"value"`
+	Description string `json:"description,omitempty"`
 }
--- postman/transformer.go
+++ postman/transformer.go
@@ -7,13 +7,53 @@ import (
 )
 
 func CreateCollection(bp *api.API) ([]byte, error) {
+	folders := make([]Folder, 0)
+	for _, resourceGroup := range bp.ResourceGroups {
+		folders = append(folders, folderFromResourceGroup(&resourceGroup))
+	}
+
 	coll := Collection{
 		Info: Information{
 			Name:      bp.Title,
 			PostmanID: bp.Title,
-			Schema:    "http://schema.getpostman.com/json/collection/v2.1.0/collection.json",
+			Schema:    "https://schema.getpostman.com/json/collection/v2.1.0/docs/index.html",
 		},
+		Items: folders,
 	}
 
 	return json.Marshal(coll)
+}
+
+func folderFromResourceGroup(rg *api.ResourceGroup) Folder {
+	items := make([]Folder, 0)
+	for _, resource := range rg.Resources {
+		items = append(items, folderFromResource(resource))
+	}
+
+	return Folder{
+		Name:  rg.Title,
+		Items: items,
+	}
+}
+
+func folderFromResource(rsc *api.Resource) Folder {
+	items := make([]Item, 0)
+	for _, transition := range rsc.Transitions {
+		items = append(items, itemFromTransition(transition))
+	}
+
+	return Folder{
+		Name:  rsc.Title,
+		Items: items,
+	}
+}
+
+func itemFromTransition(tr *api.Transition) Item {
+	return Item{
+		Name: tr.Title,
+		Request: Request{
+			Url:    tr.URL,
+			Method: tr.Method,
+		},
+	}
 }
