44e1c8bbc190dd2a033b1c8eb91c7c32880c226f
Muhammad Saiful Islam <muhammad@saiful.web.id>
2017-11-07 13:03:53 +0700

    postman: Transform n last segment of host into {{host}}

--- main.go
+++ main.go
@@ -56,6 +56,7 @@ func main() {
 	}
 
 	postman.SchemeToVariable(&collection)
+	postman.HostToVariable(&collection, 2)
 
 	json, err := json.Marshal(collection)
 	if err != nil {
--- postman/transformer.go
+++ postman/transformer.go
@@ -1,11 +1,36 @@
 package postman
 
+import (
+	"strings"
+)
+
 func SchemeToVariable(c *Collection) {
 	items := getItemsFromCollection(c)
 
 	for i := range items {
 		if items[i].Request.Url.Protocol != "" {
 			items[i].Request.Url.Protocol = "{{scheme}}"
+		}
+	}
+}
+
+func HostToVariable(c *Collection, n int) {
+	items := getItemsFromCollection(c)
+
+	for i := range items {
+		host := items[i].Request.Url.Host
+		if host != "" {
+			hostSegments := strings.Split(host, ".")
+
+			newHostSegments := []string{}
+			if len(hostSegments) > n {
+				newHostSegments = append(
+					newHostSegments,
+					hostSegments[0:len(hostSegments)-2]...,
+				)
+			}
+			newHostSegments = append(newHostSegments, "{{host}}")
+			items[i].Request.Url.Host = strings.Join(newHostSegments, ".")
 		}
 	}
 }
