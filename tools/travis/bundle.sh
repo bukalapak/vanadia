36231e2f99e4241ba276770ff036859a44077abe
Krzysztof Szafrański <krzysztof.szafranski@sharpershape.com>
2018-08-23 16:27:56 +0300

    Delete Travis files

--- tools/travis/bundle.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-TRAVIS_ARCHIVE_FILE=vanadia-$TRAVIS_TAG.${TRAVIS_OS_NAME/osx/darwin}-amd64.tar.gz
-
-if [[ "$TRAVIS_TEST_RESULT" == "0" ]]; then
-    echo "Generating ${TRAVIS_ARCHIVE_FILE}"
-    tar -czf $TRAVIS_ARCHIVE_FILE vanadia LICENSE README.md vanadia.yml
-    shasum -a 256 -b $TRAVIS_ARCHIVE_FILE
-fi
